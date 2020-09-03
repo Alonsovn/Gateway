44a5d2bc0175d4ddf85de918592fb64e2ddfd0a7
Alonsovn <alonsonh94@gmail.com>
2020-09-02 22:30:56 -0600

    Adding Pulsar Producer. Sending message to Producer working.

--- src/main/java/com/gateway/app/service/Consumer.java
+++ src/main/java/com/gateway/app/service/Consumer.java
@@ -11,11 +11,26 @@ import org.springframework.stereotype.Service;
 public class Consumer {
 
 	private final Logger logger = LoggerFactory.getLogger(Consumer.class);
+	private ProducerPulsar producerPulsar;
+	
+	
+	public Consumer() {
+		producerPulsar =  new ProducerPulsar();
+	}
+	
 
 	@KafkaListener(topics = "topic", groupId = "group_id")
 	public void consume(String message) throws IOException {
-		logger.info(String.format("#### -> Consumed message -> %s", message));
+		logger.info(String.format("#### -> Consumed message (Kafkfa) -> %s", message));
+		sendMessageProducerPulsar(message);
+		
 
 	}
+	
+	public void sendMessageProducerPulsar(String message) {
+		producerPulsar.sendMessageProducerPulsar(message);
+		
+	}
+	
 
 }
